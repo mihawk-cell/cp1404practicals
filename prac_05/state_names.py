7ddf4627a952a9b2d7cd5959e53fa5a1afc2cfdc
linyuchu <yuchu.lin@my.jcu.edu.au>
2025-10-23 18:35:59 +0800

    Use EAFP style with try/except for invalid state lookup

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,18 +5,18 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 
-for code, name in CODE_TO_NAME.items():
+for code, name in STATE_NAMES.items():
     print(f"{code:3} is {name}")
 
 print()
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    try:
+        print(STATE_NAMES[state_code])
+    except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
