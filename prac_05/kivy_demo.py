b58a6c1d6be608fe11534a2138f18e7c86eeca3e
linyuchu <yuchu.lin@my.jcu.edu.au>
2025-10-25 01:26:40 +0800

    Add bottom Label for Kivy demo assignment

--- prac_05/kivy_demo.py
+++ prac_05/kivy_demo.py
@@ -1,38 +1,23 @@
+"""
+Kivy Demo Program
+Estimate: 15 minutes
+Actual:
+"""
+
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
-from kivy.properties import StringProperty
-from kivy.uix.button import Button
 
+# Read KV file
+Builder.load_file("kivy_layout.kv")
 
-class KivyDemo(App):
-    """Kivy program to demo some basic interactive functionality."""
-    status_text = StringProperty("Hello. Click the buttons :)")
+class MyRoot(BoxLayout):
+    pass
 
-    def __init__(self, **kwargs):
-        """Construct main Kivy app."""
-        super().__init__(**kwargs)
-        self.counter = 0
-        self.names = ["Lindsay", "Osmond", "Paul"]
-
+class KivyDemoApp(App):
     def build(self):
-        """Construct the dynamic widgets."""
-        self.title = "Hello world!"
-        self.root = Builder.load_file('kivy_layout.kv')
-        for name in self.names:
-            button = Button(text=name)
-            button.bind(on_press=self.handle_name_button)
-            self.root.ids.names_box.add_widget(button)
-        return self.root
+        return MyRoot()
 
-    def handle_name_button(self, instance):
-        """Handle presses on the name button to greet people."""
-        print("Hello", instance.text)
+if __name__ == "__main__":
+    KivyDemoApp().run()
 
-    def handle_press(self, amount):
-        """Handle presses on the up/down buttons to change counter."""
-        self.counter += amount
-        self.status_text = f"The count is: {self.counter}"
-
-
-# Create an instance of the KivyDemo class and start the App running
-KivyDemo().run()
